536ca51befc8f8da44a982989a7099e8a12c4a0a
Paolo Leopardi <paolo.leopardi96@gmail.com>
2023-10-29 15:54:55 +0100

    added TSP waypoint function

--- simulator/TSP_waypoint.m
+++ simulator/TSP_waypoint.m
@@ -1,29 +1,23 @@
-% TSP example
+function [ordered_waypoint] = TSP_waypoint(waypoints)
 
-clc
-close all
-
-x = [0.5 1.0 0.3 1.2 1.5 0.0 3.4 2.1 3.1]';
-y = [0.2 1.8 0.4 0.8 1.6 0.0 2.2 0.6 4.2]';
-
-% waypoints = [x', y']
-figure
-movegui(gcf, 'center');
-scatter(x, y)
+x = waypoints(:,1);
+y = waypoints(:,2);
 
 nStops = size(x,1);
 
-idxs = nchoosek(1:nStops, 2)
+idxs = nchoosek(1:nStops, 2);
 
 dist = hypot(x(idxs(:,1)) - x(idxs(:,2)), ...
-             y(idxs(:,1)) - y(idxs(:,2)))
+             y(idxs(:,1)) - y(idxs(:,2)));
 lendist = length(dist);
 
-% create graph
 G = graph(idxs(:,1),idxs(:,2));
 figure
 movegui(gcf, 'center');
 hGraph = plot(G,'XData',x,'YData',y,'LineStyle','none','NodeLabel',{});
+grid on
+
+
 
 tsp = optimproblem; % Create optimization problem
 trips = optimvar('trips',lendist,1,'Type','integer','LowerBound',0,'UpperBound',1); % Create optimization variable
@@ -38,14 +32,14 @@ tsp.Constraints.constr2trips = constr2trips;
 
 
 opts = optimoptions('intlinprog','Display','off');
-tspsol = solve(tsp,'options',opts)
+tspsol = solve(tsp,'options',opts);
 
 tspsol.trips = logical(round(tspsol.trips));
 Gsol = graph(idxs(tspsol.trips,1),idxs(tspsol.trips,2),[],numnodes(G));
 
 hold on
 highlight(hGraph,Gsol,'LineStyle','-')
-title('Solution with Subtours')
+
 
 tourIdxs = conncomp(Gsol);
 numtours = max(tourIdxs); % Number of subtours
@@ -64,7 +58,7 @@ while numtours > 1 % Repeat until there is just one subtour
     end
     
     % Try to optimize again
-    [tspsol,fval,exitflag,output] = solve(tsp,'options',opts);
+    [tspsol, fval, exitflag, output] = solve(tsp,'options',opts);
     tspsol.trips = logical(round(tspsol.trips));
     Gsol = graph(idxs(tspsol.trips,1),idxs(tspsol.trips,2),[],numnodes(G));
     % Gsol = graph(idxs(tspsol.trips,1),idxs(tspsol.trips,2)); % Also works in most cases
@@ -80,41 +74,38 @@ while numtours > 1 % Repeat until there is just one subtour
     fprintf('# of subtours: %d\n',numtours)    
 end
 
-title('Solution with Subtours Eliminated');
 hold off
-
-disp(output.absolutegap)
 
 %%
 
-edges = Gsol.Edges.EndNodes
+edges = Gsol.Edges.EndNodes;
     
 
 starting_node = 1; % Change this to your desired starting node
-sorted_nodes = [starting_node];
+sorted_nodes = starting_node;
 
 
 while ~isempty(edges)
     
-    idx = find(edges(:,1) == starting_node | edges(:,2) == starting_node)
+    idx = find(edges(:,1) == starting_node | edges(:,2) == starting_node);
     
     if edges(idx, 1) == starting_node 
-        next_node = edges(idx, 2);   
+        next_node = edges(idx, 2);
+        next_node = next_node(1); % only for the first iteration
     else 
         next_node = edges(idx, 1);
     end
-    
-    
-    edges(idx, :) = []
+        
+    edges(idx, :) = []; % remove currente edge from the list
 
-    add = next_node(1)
+    sorted_nodes = [sorted_nodes, next_node];
 
-    sorted_nodes = [sorted_nodes, next_node(1)]
-
-    starting_node = add
+    starting_node = next_node;
 
 end
 
-disp(sorted_nodes);
- 
+ordered_waypoint = sorted_nodes;
 
+disp('Sorted nodes:')
+disp(sorted_nodes);
+end
